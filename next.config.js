836a4c1ab86a627df8b72f1dd605fdbb17ac7dcb
Masaya <masaya.sasaki.99@gmail.com>
2022-07-28 20:12:23 +0900

    added dotenv to prevent API_KEY exposure

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    API_KEY: "Mkwi1dD5QYja_dyCaS5kYfzTqRAK8FXY",
+    API_KEY: process.env.ALCHMEY_API_KEY,
   }
 }
 
